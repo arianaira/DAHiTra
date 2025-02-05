dff7d9af5e2ec226881d17cf2df2aff74af63c7a
Ariana Aghamohamadi <cogokeye@gmail.com>
2025-02-05 17:34:36 +0330

    Update run_cd.sh

--- scripts/run_cd.sh
+++ scripts/run_cd.sh
@@ -19,7 +19,7 @@ split=train
 split_val=val  
 project_name=XBD_${net_G}_${data_name}_b${batch_size}_lr${lr}_${split}_${split_val}_${max_epochs}_${lr_policy}_ce_smoothen
 
-python main_cd.py --img_size ${img_size} --checkpoint_root ${checkpoint_root} --lr_policy ${lr_policy} --split ${split} --split_val ${split_val} --net_G ${net_G} --gpu_ids ${gpus} --max_epochs ${max_epochs} --project_name ${project_name} --batch_size ${batch_size} --data_name ${data_name}  --lr ${lr} --dataset ${dataset} --loss ${loss} --n_class ${n_class}
+python /kaggle/working/DAHiTra/main_cd.py --img_size ${img_size} --checkpoint_root ${checkpoint_root} --lr_policy ${lr_policy} --split ${split} --split_val ${split_val} --net_G ${net_G} --gpu_ids ${gpus} --max_epochs ${max_epochs} --project_name ${project_name} --batch_size ${batch_size} --data_name ${data_name}  --lr ${lr} --dataset ${dataset} --loss ${loss} --n_class ${n_class}
 
 
 
